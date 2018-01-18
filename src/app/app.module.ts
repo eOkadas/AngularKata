fce8f81d28c5758ecc83690095405f0d11668bc9
Etienne Delattre <etienne.delattre@younited-credit.fr>
2018-01-18 15:44:11 +0100

    observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ItemsBoardComponent } from './items-board/items-board.component';
-import { HttpClient } from 'selenium-webdriver/http';
 
 import { HttpClientModule } from '@angular/common/http';
 
--- src/app/items-board/items-board.component.ts
+++ src/app/items-board/items-board.component.ts
@@ -9,40 +9,12 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./items-board.component.css']
 })
 export class ItemsBoardComponent implements OnInit {
-categories: Category[];
+categories: Observable<Category[]>;
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.httpClient.get<Category[]>('api/categories.json')
-        .subscribe(_ => this.categories = _);
-    // this.categories = [
-    //   {
-    //     id: 1,
-    //     name: 'Electronics',
-    //     iconClass: 'fa fa-desktop fa-4x'
-    // },
-    // {
-    //     id: 2,
-    //     name: 'Women',
-    //     iconClass: 'fa fa-female fa-4x'
-    // },
-    // {
-    //     id: 3,
-    //     name: 'Car',
-    //     iconClass: 'fa fa-car fa-4x'
-    // },
-    // {
-    //     'id': 4,
-    //     'name': 'Home',
-    //     'iconClass': 'fa fa-home fa-4x'
-    // },
-    // {
-    //     id: 5,
-    //     name: 'Books',
-    //     iconClass: 'fa fa-book fa-4x'
-    // }
-    // ];
+    this.categories = this.httpClient.get<Category[]>('api/categories.json');
   }
 
 }

5032e7526dc85ec55803668c70112ff304359be5
Etienne Delattre <etienne.delattre@younited-credit.fr>
2018-01-18 17:00:54 +0100

    appComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,32 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ItemsBoardComponent } from 'app/items-board/items-board.component';
+import { CategoryService } from 'app/services/category.service';
+import { createStubInstance } from 'sinon';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+          AppComponent
+        , ItemsBoardComponent
       ],
+      providers: [{ provide: CategoryService, useValue: createStubInstance(CategoryService) }]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('MarketPlace');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/items-board/items-board.component.spec.ts
+++ src/app/items-board/items-board.component.spec.ts
@@ -1,8 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ItemsBoardComponent } from './items-board.component';
+import { CategoryService } from 'app/services/category.service';
+import { createStubInstance } from 'sinon';
 
 describe('ItemsBoardComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+          ItemsBoardComponent
+      ],
+      providers: [{ provide: CategoryService, useValue: createStubInstance(CategoryService) }]
+    }).compileComponents();
+  }));
   let component: ItemsBoardComponent;
   let fixture: ComponentFixture<ItemsBoardComponent>;
 

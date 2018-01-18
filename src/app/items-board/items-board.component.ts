9c50eca4d8a5f4dbc55ecf62dc099e5817d839a3
Etienne Delattre <etienne.delattre@younited-credit.fr>
2018-01-18 14:57:34 +0100

    adding Categories model

--- src/app/items-board/items-board.component.ts
+++ src/app/items-board/items-board.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items-board.component.css']
 })
 export class ItemsBoardComponent implements OnInit {
+categories: Category[];
 
   constructor() { }
 
   ngOnInit() {
+    this.categories = [
+      {
+        id: 1,
+        name: 'Electronics',
+        iconClass: 'fa fa-desktop fa-4x'
+    },
+    {
+        id: 2,
+        name: 'Women',
+        iconClass: 'fa fa-female fa-4x'
+    },
+    {
+        id: 3,
+        name: 'Car',
+        iconClass: 'fa fa-car fa-4x'
+    },
+    {
+        'id': 4,
+        'name': 'Home',
+        'iconClass': 'fa fa-home fa-4x'
+    },
+    {
+        id: 5,
+        name: 'Books',
+        iconClass: 'fa fa-book fa-4x'
+    }
+    ]
   }
 
+}
+export interface Category {
+  id: number;
+  name: string;
+  iconClass: string;
 }

7ca9cc24f3909a19bb5affda25cc229bb4e65620
Etienne Delattre <etienne.delattre@younited-credit.fr>
2018-01-18 14:47:46 +0100

    init marketplace

---
+++ src/app/items-board/items-board.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemsBoardComponent } from './items-board.component';
+
+describe('ItemsBoardComponent', () => {
+  let component: ItemsBoardComponent;
+  let fixture: ComponentFixture<ItemsBoardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ItemsBoardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ItemsBoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
